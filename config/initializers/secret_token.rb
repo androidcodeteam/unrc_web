897e4c6ed64a195148ee5a67772a9fb39b04b637
saul coria <coriasaul@gmail.com>
2013-04-21 08:29:13 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnrcWeb::Application.config.secret_token = '8e624b7be566d7acefd537d3248f3bbfc665a6f208349daf2076c33322b73eba5badfb7f5a14eaad15112fecc1c55de185fb02a0601a919579350cc3f5258214'
